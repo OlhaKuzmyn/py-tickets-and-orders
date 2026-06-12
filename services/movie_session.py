e124b0a1cab052f412eb757d5cb83646c9ef127b
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-06-12 22:51:28 +0300

    added services and edited services

--- services/movie_session.py
+++ services/movie_session.py
@@ -42,3 +42,9 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    for movie_session in MovieSession.objects.filter(pk=movie_session_id).prefetch_related("tickets"):
+        row_seat = [(ticket.row, ticket.seat) for ticket in movie_session.tickets.all()]
+        return [{"row": row, "seat": seat} for row, seat in row_seat]
---
+++ services/order.py
@@ -0,0 +1,38 @@
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Ticket, Order, MovieSession
+
+
+@transaction.atomic
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: datetime.date = None,
+) -> Order:
+    order = Order.objects.create(
+        user = get_user_model().objects.get(username=username),
+    )
+    if date:
+        order.created_at = date
+    Ticket.objects.bulk_create(
+        [
+            Ticket(
+                row=ticket["row"],
+                seat=ticket["seat"],
+                movie_session=MovieSession.objects.get(pk=ticket["movie_session"]),
+                order=order
+            )
+            for ticket in tickets
+        ]
+    )
+    return order
+
+
+def get_orders(username: str = None) -> QuerySet[Order]:
+    if username:
+        return Order.objects.filter(user__username=username)
+    return Order.objects.all()
