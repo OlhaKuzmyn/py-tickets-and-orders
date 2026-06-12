e955f9eab0407542a5a87476bc0caeb427f4c9d7
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-06-13 00:12:35 +0300

    added square brackets to error msg

--- db/models.py
+++ db/models.py
@@ -100,16 +100,20 @@ class Ticket(models.Model):
     def clean(self) -> None:
         if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
             raise ValidationError({
-                "seat":
-                "seat number must be in available range: (1, seats_in_row): "
-                f"(1, {self.movie_session.cinema_hall.seats_in_row})"
+                "seat": [
+                    "seat number must be in available range: "
+                    "(1, seats_in_row): "
+                    f"(1, {self.movie_session.cinema_hall.seats_in_row})"
+                ]
             })
 
         if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
             raise ValidationError({
-                "row":
-                "row number must be in available range: (1, rows): "
-                f"(1, {self.movie_session.cinema_hall.rows})"
+                "row": [
+                    "row number must be in available range: "
+                    "(1, rows): "
+                    f"(1, {self.movie_session.cinema_hall.rows})"
+                ]
             })
 
     def save(self, *args, **kwargs) -> None:
