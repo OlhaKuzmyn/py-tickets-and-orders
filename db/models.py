9b8bf6459b0908fa6a664bf7f6418a67a1825c02
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-06-13 00:01:04 +0300

    changed str in Ticket all tests are passing

--- db/models.py
+++ db/models.py
@@ -93,7 +93,8 @@ class Ticket(models.Model):
     )
 
     def __str__(self) -> str:
-        return (f"{self.movie_session.movie.title} {self.order} "
+        return (f"{self.movie_session.movie.title} "
+                f"{self.movie_session.show_time} "
                 f"(row: {self.row}, seat: {self.seat})")
 
     def clean(self) -> None:
